62ae023fff80ca3e46a076744f7d301782b6ab80
geoff loh <geoffreyloh@gmail.com>
2016-06-03 00:50:56 +1200

    update

--- navbar.js
+++ navbar.js
@@ -9,31 +9,34 @@ if (window.addEventListener) {
   window.attachEvent("ontouchmove", function () {fix_nav(); });
   window.attachEvent("onload", function () {fix_nav(); });
 }
+document.getElementById("aboutlnk").addEventListener("click", fix_anchor);
+document.getElementById("portfoliolnk").addEventListener("click", fix_anchor);
+document.getElementById("contactlnk").addEventListener("click", fix_anchor);
+document.getElementById("topContactLnk").addEventListener("click", fix_anchor);
 
-document.getElementById("aboutlnk").addEventListener("click", fix);
-
-function fix() {
-		//alert(document.body.scrollHeight+'');
-		//alert(window.innerHeight);
-		/*if (document.body.scrollHeight - window.innerHeight - window.pageYOffset > 120) {
-			document.getElementById("navbar").style.position = "fixed"; 
-		}*/
-		//document.getElementById("aboutlnk").click(); 
-
+function fix_anchor() {
+	var h = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+	if (document.body.scrollHeight - h - window.pageYOffset > 177) {
+		document.getElementById("navbar").style.position = "fixed";
+		document.getElementById("main").style.paddingTop = "48px";
+	}
+	return;
 }
 
 function fix_nav() {
 	var top = scrolltop();
-	if (top > 120) {
-		document.getElementById("navbar").style.position = "fixed";  
+	if (top > 177) {
+		document.getElementById("navbar").style.position = "fixed";
+		document.getElementById("main").style.paddingTop = "48px";
 	} else {
 		document.getElementById("navbar").style.position = "relative";
+		document.getElementById("main").style.paddingTop = 0;
 	}
 	return;
 }
 
 function scrolltop() {
-  var top;
+  var top = 0;
   if (typeof(window.pageYOffset) == "number") {
     top = window.pageYOffset;
   } else if (document.body && document.body.scrollTop) {
--- script.js
+++ script.js
@@ -2,20 +2,13 @@
 "<p>&copy;  " + new Date().getFullYear() + " OTATOU Innovations. All rights reserved.</p>";*/
 
 document.getElementById("header").innerHTML =
-		"<ul class=navigation style='margin-left:30px;margin-bottom:30px;'>" +
-		"<li><a href='index.html'><img class='top' src='img/home.png'></a></li>" +
-		"<li style='float:right; margin-right:30px; margin-top:30px'><a href='#contact'><h1 class='top'>Contact</h1></a></li>" +
+		"<ul class=navigation style='margin-left:30px;margin-bottom:20px'>" +
+		"<li><a href=''><img class='top' src='logo.png' style='width:200px;'></a></li>" +
+		"<li style='float:right; margin-right:30px; margin-top:45px'><a id='topContactLnk' href='#contact'><h2 class='top'>Contact</h2></a></li>" +
 		"</ul>" +
 		"<ul id=navbar class=navigation>" +
-		"<li class='navli'><a id='aboutlnk' class='active' href='#about'>About</a></li>" +
-		"<li class='navli'><a href='#potfolio'>Portfolio</a></li>" +
-		"<li class='navli'><a href='#contact'>Contact</a></li>" +
+		"<li class='navli'><a id='aboutlnk' href='#about'>About</a></li>" +
+		"<li class='navli'><a id='portfoliolnk' href='#portfolio'>Portfolio</a></li>" +
+		"<li class='navli'><a id='contactlnk' href='#contact'>Contact</a></li>" +
 		"</ul>";
-
-/*document.getElementById("nav01").innerHTML =
-		"<ul id='menu'>" +
-		"<li><a href='cleversplit.html'><img src='img/clever.png'></a></li>" +
-		"<li><a href='onemanstrash.html'><img src='img/trash.png'></a></li>" +
-		"<li><a href='novel.html'><img src='img/evo.png'></a></li>" +
-		"<li><a href='tradiebin.html'><img src='img/bin.png'></a></li>" +
-		"</ul>";*/
+ 
