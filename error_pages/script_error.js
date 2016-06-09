359f23843da133d2e47c5673186662458c4c0552
unknown <U358044@W3612128.int.Corp.sun>
2016-06-10 10:14:23 +1200

    update

---
+++ error_pages/script_error.js
@@ -0,0 +1,14 @@
+/*document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " OTATOU Innovations. All rights reserved.</p>";*/
+
+document.getElementById("header").innerHTML =
+		"<ul class=navigation style='margin-left:30px;margin-bottom:20px'>" +
+		"<li><a href='/'><img class='top' src='../logo.png' style='width:200px;'></a></li>" +
+		"<li style='float:right; margin-right:30px; margin-top:45px'><a id='topContactLnk' href='/#contact'><h2 class='top'>Contact</h2></a></li>" +
+		"</ul>" +
+		"<ul id=navbar class=navigation>" +
+		"<li class='navli'><a id='aboutlnk' href='/#about'>About</a></li>" +
+		"<li class='navli'><a id='portfoliolnk' href='/#portfolio'>Portfolio</a></li>" +
+		"<li class='navli'><a id='contactlnk' href='/#contact'>Contact</a></li>" +
+		"</ul>";
+ 
